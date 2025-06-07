a0f4cd88f87eb4783617c061bb0b5a82c16ac756
Cristian Sanchez <sanchez.cn@hotmail.com>
2025-06-07 12:13:50 -0300

    Se implementa la logica para seleccionar un dado.

--- include/dados.h
+++ include/dados.h
@@ -7,4 +7,6 @@ int tirar_dado(int caras);
 
 void tirar_dados(int dados[], int cantidad, int caras);
 
+void seleccionar_dados(int dados[],int dados_seleccionados[], int cantidad);
+
 #endif // DADOS_H
--- src/dados.cpp
+++ src/dados.cpp
@@ -1,6 +1,8 @@
 #include "dados.h"
 #include <cstdlib>
 #include <ctime>
+#include <iostream>
+using namespace std;
 int tirar_dado(int caras) {
     return rand() % caras + 1;
 }
@@ -8,5 +10,18 @@ int tirar_dado(int caras) {
 void tirar_dados(int dados[], int cantidad, int caras) {
     for (int i = 0; i < cantidad; ++i) {
         dados[i] = tirar_dado(caras);
+    }
+}
+
+void seleccionar_dados(int dados[], int dados_seleccionados[], int cantidad){
+    int seleccionado = 0;
+    int i = 0;
+    cout << "Selecciona un dado en base a su posicion: ";
+    cin >> seleccionado;
+    while (seleccionado != 0 && i < cantidad) {
+        dados_seleccionados[i] = dados[seleccionado-1];
+        i++;
+        cout << "Selecciona un dado en base a su posicion: ";
+        cin >> seleccionado;
     }
 }

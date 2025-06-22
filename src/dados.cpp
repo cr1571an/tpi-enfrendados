1cd07ef46f9a4c0dc9f04ca33d44d6a5c3e55cc4
Cristian Sanchez <sanchez.cn@hotmail.com>
2025-06-21 21:52:19 -0300

    Controlar que solo se elija un sola vez un dado del set de dados de stock. Controlar que la posicion elegida se correcta.

--- src/dados.cpp
+++ src/dados.cpp
@@ -7,15 +7,12 @@ int tirar_dado(int caras) {
     return rand() % caras + 1;
 }
 
-
 void tirar_dados(int dados[], int cantidad, int caras) {
     for (int i = 0; i < cantidad; ++i) {
         dados[i] = tirar_dado(caras);
     }
 }
 
-
-/*
 void seleccionar_dados(int dados[], int dados_seleccionados[], int cantidad) {
     int seleccionado;
     int i = 0;
@@ -24,41 +21,21 @@ void seleccionar_dados(int dados[], int dados_seleccionados[], int cantidad) {
     cout << "Selecciona un dado en base a su posicion (0 para terminar): ";
     cin >> seleccionado;
 
-    while (seleccionado != 0 && i < cantidad) {
-        if (seleccionado < 1 || seleccionado > cantidad) {
-            cout << "Posicion invalida. Intenta de nuevo: ";
-            cin >> seleccionado;
-        }
-        else if (posiciones_usadas[seleccionado - 1]) {
-            cout << "No se puede repetir de dado. Por favor intenta de nuevo: ";
-            cin >> seleccionado;
-        }
+    while (seleccionado != 0) {
+        if (seleccionado < 1 || seleccionado > cantidad) 
+            cout << "Posicion invalida. Intenta de nuevo: "<< endl;            
+        else if (posiciones_usadas[seleccionado - 1])
+            cout << "No se puede repetir de dado. Por favor intenta de nuevo: "<< endl;
         else {
-            dados_seleccionados[seleccionado - 1] = dados[seleccionado - 1];
+            dados_seleccionados[i] = dados[seleccionado - 1];
             posiciones_usadas[seleccionado - 1] = true;
-            i++;
-            cout << "Selecciona un dado en base a su posicion (0 para terminar): ";
-            cin >> seleccionado;
+            i++;            
         }
-    }
-}
-*/
-
-/// BORRAR DESPUES DE PROBAR EL CODIGO. ESTA FUCION DE SELECCION DE DADOS SE PUEDE USAR EL MISMO DADO MUCHA VECES.
-/// Y DESCOMENTAR LA OTRA FUNCION DE ARRIBA.
-void seleccionar_dados(int dados[], int dados_seleccionados[], int cantidad){
-    int seleccionado = 0;
-    int i = 0;
-    cout << "Selecciona un dado en base a su posicion: ";
-    cin >> seleccionado;
-    while (seleccionado != 0 && i < cantidad) {
-        dados_seleccionados[i] = dados[seleccionado-1];
-        i++;
-        cout << "Selecciona un dado en base a su posicion: ";
+        
+        cout << "Selecciona un dado en base a su posicion (0 para terminar): ";
         cin >> seleccionado;
     }
 }
-
 
 int sumar_dados(int dados[], int cantidad) {
     int suma = 0;
